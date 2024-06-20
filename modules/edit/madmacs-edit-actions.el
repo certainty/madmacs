399ebc21e86e2ffe43743a11a5c2754c4a950bb9
certainty <david.krentzlin@gmail.com>
2024-06-20 07:43:53 +0200

    Remove unused configuration

--- modules/edit/madmacs-edit-actions.el
+++ modules/edit/madmacs-edit-actions.el
@@ -1,8 +1,12 @@
+;; -*- lexical-binding: t; -*-
+;; edit actions
+;; this is a collection of actions that can be used to manipulate text in the buffer
+
 (use-package embark
   :ensure t
   :bind
-  (("C-." . embark-act) ; this is ok since it gives us xrefs as options
-   ("M-." . embark-dwim) ; this is also ok since it also gives us xrefs as options
+  (("C-." . embark-act)                 ; this is ok since it gives us xrefs as options
+   ("M-." . embark-dwim)                ; this is also ok since it also gives us xrefs as options
    ("C-h B" . embark-bindings)
    (:map madmacs-actions-map
          ("a" . embark-act)
@@ -19,44 +23,11 @@
   
   ;; by default we use completing read instead of which-key like selection
   (setopt embark-prompter 'embark-completing-read-prompter)
-  
-  (defun embark-which-key-indicator ()
-    "An embark indicator that displays keymaps using which-key.
-The which-key help message will show the type and value of the
-current target followed by an ellipsis if there are further
-targets."
-    (lambda (&optional keymap targets prefix)
-      (if (null keymap)
-          (which-key--hide-popup-ignore-command)
-        (which-key--show-keymap
-         (if (eq (plist-get (car targets) :type) 'embark-become)
-             "Become"
-           (format "Act on %s '%s'%s"
-                   (plist-get (car targets) :type)
-                   (embark--truncate-target (plist-get (car targets) :target))
-                   (if (cdr targets) "…" "")))
-         (if prefix
-             (pcase (lookup-key keymap prefix 'accept-default)
-               ((and (pred keymapp) km) km)
-               (_ (key-binding prefix 'accept-default)))
-           keymap)
-         nil nil t (lambda (binding)
-                     (not (string-suffix-p "-argument" (cdr binding))))))))
 
   (setq embark-indicators
         '(embark-which-key-indicator
           embark-highlight-indicator
-          embark-isearch-highlight-indicator))
-
-  (defun embark-hide-which-key-indicator (fn &rest args)
-    "Hide the which-key indicator immediately when using the completing-read prompter."
-    (which-key--hide-popup-ignore-command)
-    (let ((embark-indicators
-           (remq #'embark-which-key-indicator embark-indicators)))
-      (apply fn args)))
-
-  (advice-add #'embark-completing-read-prompter
-              :around #'embark-hide-which-key-indicator))
+          embark-isearch-highlight-indicator)))
 
 (use-package embark-consult
   :hook (embark-collect-mode . consult-preview-at-point-mode)
