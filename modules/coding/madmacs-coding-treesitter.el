69394ffdd4a5fb24538a5f53b87a4631ad8b552e
certainty <david.krentzlin@gmail.com>
2024-07-26 08:41:49 +0200

    Make sure treesitter works for scala
    Turns out you need to tell treesitter the depth to
    which constructs are treated for highlighting apparently.
    
    This fixes the incorrect highlighting of scala code

--- modules/coding/madmacs-coding-treesitter.el
+++ modules/coding/madmacs-coding-treesitter.el
@@ -3,11 +3,13 @@
 (use-package treesit-auto
     :ensure t
     :custom
+    (treesit-font-lock-level 4) ;; make sure we get the elements highlighted we're interested in
     (treesit-auto-install 'prompt)
     (treesit-language-source-alist
      '((elisp "https://github.com/Wilfred/tree-sitter-elisp")
        (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp"))
-       (markdown "https://github.com/ikatyang/tree-sitter-markdown"))
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (bash "https://github.com/tree-sitter/tree-sitter-bash/"))
     (treesit-auto-langs '(ruby go gomod scala rust typescript javascript yaml dockerfile json html css lua elisp))
     :config
     (global-treesit-auto-mode))
