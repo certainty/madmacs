d21672b818bf5e8cf565417e6a2b717ae3486a67
certainty <david.krentzlin@gmail.com>
2024-11-27 09:26:32 +0100

    cleanup

--- modules/coding/madmacs-coding-lsp.el
+++ modules/coding/madmacs-coding-lsp.el
@@ -1,6 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-
 ;;; Eglot
 (use-package eglot
   :ensure nil
@@ -13,7 +12,6 @@
 
   :config
   (setopt eldoc-echo-area-use-multiline-p 1))
-
 
 (defun madmacs--lsp (&rest args)
   "If LSP is eglot, call eglot-ensure, otherwise call lsp-deferred."
