987b9388ad3b34f69e17b7288b4993e52c9f81aa
certainty <david.krentzlin@gmail.com>
2024-12-15 14:26:29 +0100

    Remove old init files

--- early-init.old.el
+++
@@ -1,8 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(load (expand-file-name "setup/madmacs-bootstrap" user-emacs-directory) nil t)
-
-;; Tweak some very basic settings
-;(setopt madmacs-debug t)
-(madmacs--setup-env)
-(madmacs--boot-early)
