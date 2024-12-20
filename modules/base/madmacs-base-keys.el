eb924c0ca226a093ce51b2cb8008eea2b23e74c0
certainty <david.krentzlin@gmail.com>
2024-12-20 09:42:12 +0100

    Only reset the boon frame when the target frame is not a child frame

--- modules/base/madmacs-base-keys.el
+++ modules/base/madmacs-base-keys.el
@@ -6,7 +6,7 @@
   (bind-key-describe-special-forms nil))
 
 (use-package which-key
-  :straight nil
+  :straight (:type built-in)
   :demand t
   :custom
   (which-key-sort-order 'which-key-prefix-then-key-order)
@@ -33,7 +33,7 @@
   :config
   (dolist (command '(next-error previous-error))
     (put command 'repeat-exit-timeout 'no))
-  
+
   :bind
   (:map goto-map
     ("]" . next-buffer)
@@ -42,15 +42,15 @@
   (:repeat-map madmacs-buffer-repeat-map
     ("]" . next-buffer)
     ("[" . previous-buffer))
-  
-  ;; Object-specific Motion Repeat 
+
+  ;; Object-specific Motion Repeat
   (:repeat-map madmacs-line-repeat-map
     ("^" . join-line))
 
   (:repeat-map madmacs-paragraph-repeat-map
     ("}" . forward-paragraph)
     ("{" . backward-paragraph))
-  
+
   (:repeat-map madmacs-undo-repeat-map
     ("u" . undo))
 
@@ -61,7 +61,7 @@
 
   (:repeat-map madmacs-windows-repeat-map
     ("o" . other-window))
-  
+
   :init
   (defun madmacs--repeatize (keymap)
     "Add `repeat-mode' support to an existing KEYMAP.
@@ -79,7 +79,7 @@
   (:map madmacs-mode-map
 	("C-z" . repeat) ; I don't use suspend frame so this is a nicer binding than C-x z
 	("C-x z" . repeat-complex-command)) ; now this can go here
-  
+
   (:map madmacs-keymap-packages
     ("u" . straight-pull-package)
     ("U" . straight-pull-all))
@@ -89,12 +89,12 @@
   :config
   (unbind-key "M-g n")
   (unbind-key "C-j")
-  
+
   (defvar-keymap madmacs-keymap-global :doc "The keymap used for global commands. This is the home of keys that don't live in other global maps like C-x or C-c")
   (defvar-keymap madmacs-keymap-ux :doc "UX settings")
   (defvar-keymap madmacs-keymap-packages :doc "Package functionality")
   (defvar-keymap madmacs-keymap-ai :doc "AI related functionality")
-  
+
   (which-key-add-keymap-based-replacements madmacs-keymap-global
     "p" `("Packages" . ,madmacs-keymap-packages)
     "u" `("UX" . ,madmacs-keymap-ux)
@@ -102,7 +102,7 @@
 
   (which-key-add-keymap-based-replacements global-map
     "C-j" `("Madmacs" . ,madmacs-keymap-global))
-  
+
   (which-key-add-keymap-based-replacements madmacs-mode-map
     "C-j" `("Madmacs" . ,madmacs-keymap-global)))
 
@@ -123,26 +123,33 @@
     ("V" . insert-register)
     (":" . bookmark-set)
     ("#" . point-to-register))
-  
+
   (:map boon-forward-search-map
     ("o" . occur)
     ("c" . nil)
     ("k" . nil))
-  
+
   (:map boon-backward-search-map
     ("o" . occur)
     ("c" . nil)
     ("k" . nil))
-  
+
   (:map boon-goto-map
     ("e" . nil)
     (":" . nil)
     ("w" . ace-window)
     ("f" . project-find-file)) ; restore sane bingings
-  
+
   :config
   (require 'boon-emacs)
-  ;(remove-hook 'window-selection-change-functions #'boon-reset-state-for-switchw)
+  
+  ;; adivce the boon-reset-state-for-switchw function
+  (defun madmacs-reset-state-unless-target-is-corfu-overlay (orig-fn frame)
+    (unless (frame-parent frame)
+      ;; no parent means this is not a child frame so we can reset
+      (funcall orig-fn frame)))
+  
+  (advice-add 'boon-reset-state-for-switchw :around #'madmacs-reset-state-unless-target-is-corfu-overlay)
   
   (add-to-list 'global-mode-string
     '("%e" (:eval (boon-modeline-string))))
