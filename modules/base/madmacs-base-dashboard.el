df7d72b066ec739c46439ba692278f9c6afc08c0
certainty <david.krentzlin@gmail.com>
2024-12-20 09:42:26 +0100

    Cleanup the dashboard config

--- modules/base/madmacs-base-dashboard.el
+++ modules/base/madmacs-base-dashboard.el
@@ -1,10 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package dashboard
-  :defer nil
-  :hook (emacs-startup . (lambda ()
-                           (dashboard-refresh-buffer)
-                           (madmacs--dashboard-hide-modeline)))
   :custom
   (dashboard-banner-logo-title
     "       This is my emacs config. There are many like it, but this one is mine.
@@ -18,20 +14,8 @@
   (dashboard-startup-banner (concat user-emacs-directory "assets/logo.png"))
   (dashboard-projects-backend 'project-el)
 
-  :config
-  (defun madmacs--dashboard-hide-modeline ()
-    (interactive)
-    (let ((dash-buffer (get-buffer "*dashboard*")))
-      (when dash-buffer
-        (with-current-buffer dash-buffer
-          (setq-local hl-line-mode -1)
-          (setq-local mode-line-format nil)
-          (setq-local header-line-format nil)))))
+  :init
+  (dashboard-setup-startup-hook))
 
-  (defun madmacs-goto-dashboard ()
-    (interactive)
-    (dashboard-refresh-buffer)
-    (switch-to-buffer "*dashboard*")
-    (delete-other-windows)))
 
 (provide 'madmacs-base-dashboard)
