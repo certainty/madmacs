b583666cbd770a8c60bf663eb6ecaac0dfa18c20
certainty <david.krentzlin@gmail.com>
2025-01-02 14:49:24 +0100

    make sure builtin packages remain builtin

---
+++ modules/base/madmacs-base-pin-builtins.el
@@ -0,0 +1,11 @@
+;; -*- lexical-binding: t; -*-
+
+;; inform straight about builtin packages
+
+(use-package project
+  :straight (:type built-in))
+
+(use-package whichkey
+  :straight (:type built-in))
+
+(provide 'madmacs-base-pin-builtins)
